c396b39acc96ae87ec7378ff758cddb7c904f0ca
Luis <lechavero.24@est.ucab.edu.ve>
2026-07-22 09:44:25 -0400

    setup inicial

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // genera el schema automáticamente
+      sortSchema: true,
+      playground: true, // interfaz para probar queries en desarrollo
+    }),
+  ],
+})
+export class AppModule {}
